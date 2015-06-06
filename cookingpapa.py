1f41a5ef7af1ed5c9694fb329a20030903a06505
keigo yamamoto <k5.trismegitus@gmail.com>
2015-06-06 11:52:35 +0900

    task6

--- cookingpapa.py
+++ cookingpapa.py
@@ -4,7 +4,7 @@ import sys
 import csv
 
 
-def load_recipe_csv(csv_name):
+def load_recipe_data(csv_name):
 
     recipe_list = []
     reader = csv.reader(open(csv_name, mode='r', encoding='utf-8'))
@@ -20,12 +20,27 @@ def load_recipe_csv(csv_name):
     return recipe_list
 
 
-def print_recipe(recipe_list):
+def print_recipe_all(recipe_list):
     for recipe in recipe_list:
         print(recipe['recipe_id'] + ': ' + recipe['recipe_name'])
 
 
+def print_recipe_by_id(recipe_list, wanted_id):
+    for recipe in recipe_list:
+        if wanted_id == recipe['recipe_id']:
+            print(recipe['recipe_id'] + ': ' + recipe['recipe_name'])
+
+
 if __name__ == '__main__':
     recipe_csv = sys.argv[1]
-    recipe_list = load_recipe_csv(recipe_csv)
-    print_recipe(recipe_list)
+    if len(sys.argv) == 3:
+        wanted_recipe_id = sys.argv[2]
+    else:
+        wanted_recipe_id = None
+
+    recipe_list = load_recipe_data(recipe_csv)
+
+    if wanted_recipe_id:
+        print_recipe_by_id(recipe_list, wanted_recipe_id)
+    else:
+        print_recipe_all(recipe_list)
