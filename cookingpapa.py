55fe391fb753daa8bdad3d5c4d0d1e44ee7ebbc4
keigo yamamoto <k5.trismegitus@gmail.com>
2015-06-06 11:21:21 +0900

    task3 done

--- cookingpapa.py
+++ cookingpapa.py
@@ -1,4 +1,17 @@
 __author__ = 'keigo'
 
+import sys
+import csv
+
+
+def load_recipe_csv(csv_name):
+    reader = csv.reader(open(csv_name, mode='r', encoding='utf-8'))
+    header = next(reader)
+
+    for row in reader:
+        print(row)
+
+
 if __name__ == '__main__':
-    print('オムライス')
+    recipe_csv = sys.argv[1]
+    load_recipe_csv(recipe_csv)
