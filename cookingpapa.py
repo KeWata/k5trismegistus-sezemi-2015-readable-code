aa93d2fda1fa711a336537330eea33eb2be9a3fa
Keita Watanabe <092975@gmail.com>
2015-06-06 14:46:49 +0900

    URL情報を追加

--- cookingpapa.py
+++ cookingpapa.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'keigo'
 
 import sys
@@ -7,13 +8,14 @@ import csv
 def load_recipe_data(csv_name):
 
     recipe_list = []
-    reader = csv.reader(open(csv_name, mode='r', encoding='utf-8'))
+    reader = csv.reader(open(csv_name, mode='r'))
     header = next(reader)
 
-    for row in reader:
+    for idx, row in enumerate(reader):
         recipe = {
-            'recipe_id': row[0],
-            'recipe_name': row[1],
+            'recipe_id': str(idx),
+            'recipe_name': row[0],
+            'recipe_URL' : row[1]
         }
         recipe_list.append(recipe)
 
@@ -22,13 +24,13 @@ def load_recipe_data(csv_name):
 
 def print_recipe_all(recipe_list):
     for recipe in recipe_list:
-        print(recipe['recipe_id'] + ': ' + recipe['recipe_name'])
+        print(recipe['recipe_id'] + ': ' + recipe['recipe_name'] + ': '+ recipe['recipe_URL'])
 
 
 def print_recipe_by_id(recipe_list, wanted_id):
     for recipe in recipe_list:
         if wanted_id == recipe['recipe_id']:
-            print(recipe['recipe_id'] + ': ' + recipe['recipe_name'])
+            print(recipe['recipe_id'] + ': ' + recipe['recipe_name']+ ': '+recipe['recipe_URL'])
 
 
 if __name__ == '__main__':
